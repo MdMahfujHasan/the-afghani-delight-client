696924c9246d93651208ce3327ef5b6b2d7bfba5
Mahfuj Munna <mahfujmunna17@gmail.com>
2023-05-26 02:32:28 +0600

    react-to-pdf added

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import banner from '../../assets/banner.jpg';
 import { GiMeal } from 'react-icons/gi';
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
     const bannerFont = {
@@ -13,7 +14,7 @@ const Banner = () => {
                 <div style={bannerFont}>
                     <h1 className="mb-4 text-3xl sm:text-4xl md:text-5xl font-extrabold">Immerse yourself in the rich heritage of Afghan cuisine, where every bite tells a story of tradition and flavor, at The Afghani Delight. <GiMeal className='text-7xl inline' /></h1>
                     <p className="mb-5"></p>
-                    <button className="btn btn-accent">Let's Get Started </button>
+                    <Link to="/sign-up"><button className="btn btn-accent">Let's Get Started </button></Link>
                 </div>
             </div>
         </div>
